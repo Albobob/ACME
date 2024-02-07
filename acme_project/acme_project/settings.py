7f6a4a18c2c3cfd79458390ae1bdfb0e00ef19cf
Albobob <treblarpip@gmail.com>
2024-02-07 14:05:15 +0300

    Добавил templates и обработку страницы SMP

--- acme_project/acme_project/settings.py
+++ acme_project/acme_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'smp.apps.SmpConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -51,10 +52,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'acme_project.urls'
 
+TEMPLATES_DIR = BASE_DIR / 'templates'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- acme_project/smp/views.py
+++ acme_project/smp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def smp(request):
+    template = 'smp/base.html'
+    return render(request, template)
